ae91a9cd95332f453ed432263c9148aba9016399
Natnael_Yigzaw <127339716+Natnael-Yigzaw@users.noreply.github.com>
2024-01-07 17:27:01 +0000

    Update Profile page with logout functionalite.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { getAuth } from "firebase/auth";
+
 const Profile = () => {
+  const auth = getAuth();
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  const { name, email } = formData;
+
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    auth.signOut();
+    navigate("/");
+  };
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={onLogout}>
+          LogOut
+        </button>
+      </header>
     </div>
   );
 };
